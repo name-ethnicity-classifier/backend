45f9c264740f1cd00accdff138fb915ec3e1eb82
Peifer, Theodor <theodor.peifer@hm.edu>
2024-04-24 22:28:42 +0200

    add route and test for deleting users

--- src/schemas/user_schema.py
+++ src/schemas/user_schema.py
@@ -2,15 +2,20 @@ from pydantic import BaseModel
 
 
 class LoginSchema(BaseModel):
-    """ Model schema to validate the /login POST request data """ 
+    """ Schema to validate the /login POST request data """ 
     email: str
     password: str
 
 
 class SignupSchema(BaseModel):
-    """ Model schema to validate the /signup POST request data """
+    """ Schema to validate the /signup POST request data """
     email: str
     password: str
     name: str
     role: str
     consented: bool
+
+
+class DeleteUser(BaseModel):
+    """ Schema to validate the /delete-user POST request data """
+    password: str
