f7132e75620eb4eeac18edf5416435641b4e249c
Peifer, Theodor <theodor.peifer@hm.edu>
2025-05-17 11:46:28 +0200

    🎉 release v1.0

--- api-documentation/sidebars.ts
+++ api-documentation/sidebars.ts
@@ -12,7 +12,7 @@ const sidebars: SidebarsConfig = {
       link: {
         type: "generated-index",
         title: "N2E API",
-        description: "This is the official REST API documentation for www.name-to-ethnicity.com. Using our public API you can classify names into their most likely ethnicity. We do not recommend you to use the API as part of your backend or any kind of deployed service. It's mainly meant for doing experiments and to be included in, for example, your Python or R scripts.",
+        description: "This is the official REST API documentation for www.name-to-ethnicity.com. Using our public API you can classify names into their most likely ethnicity. Do not use our API as part of your backend or any kind of deployed service. It is meant for doing experiments and to be included in, for example, your Python Notebooks or R scripts.",
         slug: "/"
       },
       items: require("./docs/n2e/sidebar.js")
